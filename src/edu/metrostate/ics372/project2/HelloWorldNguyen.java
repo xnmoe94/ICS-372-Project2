64ce25eb05b435628f27ee8b26f9e005c33104a5
dnguyen000 <don.nguyen.000@gmail.com>
2019-11-11 14:34:02 -0600

    initial commit

---
+++ src/edu/metrostate/ics372/project2/HelloWorldNguyen.java
@@ -0,0 +1,7 @@
+package edu.metrostate.ics372.project2;
+
+public class HelloWorldNguyen {
+	public static void main(String[] args) {
+		System.out.println("Hello Don Nguyen!");
+	}
+}
---
+++ test/HelloWorldTemplateTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTemplateTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
