0edd019c487188f648535a37db9a288eacf81b60
dnguyen000 <don.nguyen.000@gmail.com>
2019-11-22 17:23:44 -0600

    adding packages for project

---
+++ src/edu/metrostate/ics372/project2/buttons/GUIButtons.java
@@ -0,0 +1,21 @@
+package edu.metrostate.ics372.project2.buttons;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+public abstract class GUIButton extends Button implements EventHandler<ActionEvent> {
+    /**
+     * Create the button with the proper text. Makes the button a listener to
+     * clicks on itself.
+     * 
+     * @param string
+     *            the text
+     */
+
+    public GUIButton(String string) {
+        super(string);
+        setOnAction(this);
+    }
+
+}
