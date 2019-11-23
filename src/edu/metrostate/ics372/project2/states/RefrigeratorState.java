0c68e0669d147ff1e31d5403a56708f23a27170f
dnguyen000 <don.nguyen.000@gmail.com>
2019-11-22 18:22:23 -0600

    updated build path

--- src/edu/metrostate/ics372/project2/states/RefrigeratorState.java
+++ src/edu/metrostate/ics372/project2/states/RefrigeratorState.java
@@ -1,5 +1,32 @@
 package edu.metrostate.ics372.project2.states;
 
+import edu.metrostate.ics372.project2.events.DoorCloseEvent;
+import edu.metrostate.ics372.project2.events.DoorOpenEvent;
+
 public abstract class RefrigeratorState {
+	
+    /**
+     * Initializes the state
+     */
+    public abstract void enter();
+
+    /**
+     * Performs any necessary clean up while leaving the state
+     */
+    public abstract void leave();
+
+    /**
+     * Process door open request
+     */
+    public void handleEvent(DoorOpenEvent event) {
+
+    }
+
+    /**
+     * Process door close request
+     */
+    public void handleEvent(DoorCloseEvent event) {
+
+    }
 
 }
